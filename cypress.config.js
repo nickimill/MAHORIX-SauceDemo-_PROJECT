eea826a9d22ae7e918d04d547aa769dfe84ca04a
Faith Idebi <faithidebi@gmail.com>
2025-11-12 11:45:25 +0100

    new commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   e2e: {
+    watchForFileChanges: true,
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
        projectId: "g9ogzo"
-        video: true
     },
   },
 };
